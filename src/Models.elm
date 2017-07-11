68b4d3a68e4b93a63cfa4f3b09f3d3df791ad4e0
Christian Todd <christiantodd27@icloud.com>
2017-07-10 22:17:39 -0400

    complete resources chapter.

--- src/Models.elm
+++ src/Models.elm
@@ -2,4 +2,22 @@ module Models exposing (..)
 
 
 type alias Model =
+    { players : List Player
+    }
+
+
+initalModel : Model
+initalModel =
+    { players = [ Player "1" "Christian" 1 ]
+    }
+
+
+type alias PlayerId =
     String
+
+
+type alias Player =
+    { id : PlayerId
+    , name : String
+    , level : Int
+    }
